e7052b4baaf858e441e2b533a0bb23ab5524b28e
LiLiLingTan <minibinks@yahoo.com>
2019-11-13 09:41:34 +0800

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ChartModule } from 'angular-highcharts';
+import { HIGHCHARTS_MODULES, ChartModule } from 'angular-highcharts';
 
 import { AppComponent } from './app.component';
 
